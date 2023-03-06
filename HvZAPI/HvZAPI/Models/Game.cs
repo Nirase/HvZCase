4f0928988317cf446e21b65add460a8f40bb6639
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-06 14:50:26 +0100

    fix: Add Description to game model
    Fixes #1

--- HvZAPI/HvZAPI/Models/Game.cs
+++ HvZAPI/HvZAPI/Models/Game.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public string GameState { get; set; }
 
         public ICollection<Player> Players { get; set; }
