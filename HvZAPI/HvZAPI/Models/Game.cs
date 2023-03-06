c6a350227efcdb507ba5ed376bf43519270a1bd1
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-06 14:38:01 +0100

    feat: Add game model
    WIP #1

--- HvZAPI/HvZAPI/Models/Game.cs
+++ HvZAPI/HvZAPI/Models/Game.cs
@@ -2,5 +2,12 @@
 {
     public class Game
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string GameState { get; set; }
+
+        public ICollection<Player> Players { get; set; }
+        public ICollection<Kill> Kills { get; set; }
+
     }
 }
