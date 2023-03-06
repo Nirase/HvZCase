ba3f840d5d8e91275ed3ad9d7e1727bb2ae59a8c
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-06 14:36:30 +0100

    feat: Add User model
    WIP #1

--- HvZAPI/HvZAPI/Models/User.cs
+++ HvZAPI/HvZAPI/Models/User.cs
@@ -4,5 +4,14 @@ namespace HvZAPI.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        
+        [MaxLength(30)]
+        public string first_name { get; set; }
+
+        [MaxLength(30)]
+        public string last_name { get; set; }
+
+        ICollection<Player> players { get; set; }
     }
 }
