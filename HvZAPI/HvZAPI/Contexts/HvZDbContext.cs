311ed11d93f527ef39001f37b482961a239811e3
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-06 15:27:29 +0100

    feat: Base database creation
    Adds tables to database
    WIP #16

---
+++ HvZAPI/HvZAPI/Contexts/HvZDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HvZAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HvZAPI.Contexts
+{
+    public class HvZDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Kill> Kills { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public HvZDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
