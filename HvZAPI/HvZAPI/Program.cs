78024cd3030db264fc7c701348be8da8588e5546
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-07 09:09:26 +0100

    feat: Add transient for gameservice

--- HvZAPI/HvZAPI/Program.cs
+++ HvZAPI/HvZAPI/Program.cs
@@ -1,20 +1,30 @@
 using HvZAPI.Contexts;
+using HvZAPI.Services.Concrete;
+using HvZAPI.Services.Interfaces;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<HvZDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<HvZDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IGameService, GameService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
