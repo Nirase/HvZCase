cef527c748b3c5a528e4748e5cd747944ca694aa
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-08 13:10:05 +0100

    feat: Create basic kill service
    Creates a basic kill service that allows for adding and getting kills. Does not do anything to the player yet
    WIP #5

---
+++ HvZAPI/HvZAPI/Services/Concrete/KillService.cs
@@ -0,0 +1,37 @@
+﻿using HvZAPI.Contexts;
+using HvZAPI.Models;
+using HvZAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HvZAPI.Services.Concrete
+{
+    public class KillService : IKillService
+    {
+        private readonly HvZDbContext _context;
+        public KillService(HvZDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Kill> CreateKill(Kill kill)
+        {
+            _context.Kills.Add(kill);
+            await _context.SaveChangesAsync();
+            return kill;
+        }
+
+        public async Task<Kill> GetKillById(int id)
+        {
+            var kill = await _context.Kills.Include(x => x.Victim).Include(x => x.Killer).FirstOrDefaultAsync();
+            if (kill is null)
+                throw new Exception("Kill Not Found");
+            return kill;
+        }
+
+        public async Task<IEnumerable<Kill>> GetKills()
+        {
+            return await _context.Kills.Include(x => x.Victim).Include(x => x.Killer).ToListAsync();
+        }
+
+    }
+}
---
+++ HvZAPI/HvZAPI/Services/Interfaces/IKillService.cs
@@ -0,0 +1,28 @@
+﻿using HvZAPI.Models;
+
+namespace HvZAPI.Services.Interfaces
+{
+    public interface IKillService
+    {
+
+        /// <summary>
+        /// Fetches all Kills
+        /// </summary>
+        /// <returns>Enumerable of Kills</returns>
+        Task<IEnumerable<Kill>> GetKills();
+
+        /// <summary>
+        /// Fetches Kill based on id
+        /// </summary>
+        /// <param name="id">Kill Id to find</param>
+        /// <returns>Found Kill entity</returns>
+        Task<Kill> GetKillById(int id);
+
+        /// <summary>
+        /// Creates a new Kill entity
+        /// </summary>
+        /// <param name="Kill">Kill to create</param>
+        /// <returns>Created Kill entity</returns>
+        Task<Kill> CreateKill(Kill Kill);
+    }
+}
