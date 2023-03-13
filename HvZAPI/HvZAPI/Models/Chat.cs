465f0fa13c0728e8ebc885d35fa18cc24f001b5c
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-13 11:41:48 +0100

    feat: Add chat model

---
+++ HvZAPI/HvZAPI/Models/Chat.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HvZAPI.Models
+{ 
+
+    [PrimaryKey(nameof(Channel), nameof(GameId))]
+    public class Chat
+    {
+        public string Channel { get; set; }
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        public ICollection<ChatMessage> Messages { get; set; }
+    }
+}
