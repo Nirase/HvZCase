7b42f0f0a89c8662f0d135fa6c3471f163342c92
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-13 11:48:33 +0100

    feat: Add initial chat message model
    WIP #37

--- HvZAPI/HvZAPI/Models/ChatMessage.cs
+++ HvZAPI/HvZAPI/Models/ChatMessage.cs
@@ -2,5 +2,12 @@
 {
     public class ChatMessage
     {
+        public int Id { get; set; }
+        public Chat Chat { get; set; }
+        public string Channel { get; set; }
+        public int GameId { get; set; }
+        public Player Sender { get; set; }
+        public int PlayerId { get; set; }
+        public string Contents { get; set; }
     }
 }
