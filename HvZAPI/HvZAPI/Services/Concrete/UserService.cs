91a73e6d35f979e2b53f12ca531196d6f6dbf6c6
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-15 09:15:51 +0100

    feat: Create UserService implementation
    WIP #4

---
+++ HvZAPI/HvZAPI/Services/Concrete/UserService.cs
@@ -0,0 +1,33 @@
+﻿using HvZAPI.Models;
+using HvZAPI.Services.Interfaces;
+
+namespace HvZAPI.Services.Concrete
+{
+    public class UserService : IUserService
+    {
+        public Task<User> AddUser(User User)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUser(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUser(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateUser(User User)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
