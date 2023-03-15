54d5cd0aa52156a2b370d6b321a1c480e7a5033a
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-15 11:34:04 +0100

    feat: Add Squad Check In Model
    FIXES #45

--- HvZAPI/HvZAPI/Models/SquadCheckIn.cs
+++ HvZAPI/HvZAPI/Models/SquadCheckIn.cs
@@ -1,7 +1,13 @@
-﻿namespace HvZAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HvZAPI.Models
 {
     public class SquadCheckIn
     {
         public int Id { get; set; }
+        public int SquadId { get; set; }
+        public Squad Squad { get; set; }
+        [MaxLength(150)]
+        public string Location { get; set; }
     }
 }
