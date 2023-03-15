e9c0c3ff9fb7d4fa1606c592b8b7eb37c0ec6177
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-15 09:17:48 +0100

    feat: Add User Controller
    WIP #4

---
+++ HvZAPI/HvZAPI/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HvZAPI.Models.DTOs.UserDTOs;
+using HvZAPI.Models;
+using HvZAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Net.Mime;
+
+namespace HvZAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+    }
+}
