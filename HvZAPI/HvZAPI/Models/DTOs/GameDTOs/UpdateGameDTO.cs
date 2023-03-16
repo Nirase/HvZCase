244949fe73ce69ec78a5f5b474feef966a5d5671
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-16 13:09:16 +0100

    fix: Add more information to updategamedto
    FIXES #53

--- HvZAPI/HvZAPI/Models/DTOs/GameDTOs/UpdateGameDTO.cs
+++ HvZAPI/HvZAPI/Models/DTOs/GameDTOs/UpdateGameDTO.cs
@@ -3,6 +3,12 @@
     public class UpdateGameDTO
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public string GameState { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public float Radius { get; set; }
+        public string Location { get; set; }
     }
 }
