1b61a254cb5e200c2c3527acad58f2d1f119c41a
Mattias Smedman <mattias.smedman@gmail.com>
2023-03-16 09:33:14 +0100

    feat: Add Squad CheckIn Interface
    WIP #47

---
+++ HvZAPI/HvZAPI/Services/Concrete/SquadCheckInService.cs
@@ -0,0 +1,33 @@
+﻿using HvZAPI.Models;
+using HvZAPI.Services.Interfaces;
+
+namespace HvZAPI.Services.Concrete
+{
+    public class SquadCheckInService : ISquadCheckInService
+    {
+        public Task<SquadCheckIn> CreateSquadCheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSquadCheckIn(int SquadCheckInId, int gameId, int squadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SquadCheckIn> GetSquadCheckInById(int id, int gameId, int squadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SquadCheckIn>> GetSquadCheckIns(int gameId, int squadId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SquadCheckIn> UpdateSquadCheckIn(SquadCheckIn SquadCheckIn, int gameId, int squadId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HvZAPI/HvZAPI/Services/Interfaces/ISquadCheckInService.cs
@@ -0,0 +1,39 @@
+﻿namespace HvZAPI.Services.Interfaces
+{
+    public interface ISquadCheckInService
+    {
+        /// <summary>
+        /// Fetches all SquadCheckIns
+        /// </summary>
+        /// <returns>Enumerable of SquadCheckIns</returns>
+        Task<IEnumerable<SquadCheckIn>> GetSquadCheckIns(int gameId, int squadId);
+
+        /// <summary>
+        /// Fetches SquadCheckIn based on id
+        /// </summary>
+        /// <param name="id">SquadCheckIn Id to find</param>
+        /// <returns>Found SquadCheckIn entity</returns>
+        Task<SquadCheckIn> GetSquadCheckInById(int id, int gameId, int squadId);
+
+        /// <summary>
+        /// Creates a new SquadCheckIn entity
+        /// </summary>
+        /// <returns>Created SquadCheckIn entity</returns>
+        Task<SquadCheckIn> CreateSquadCheckIn();
+
+        /// <summary>
+        /// Deletes an existing SquadCheckIn entity
+        /// </summary>
+        /// <param name="SquadCheckInId">SquadCheckIn entity to delete</param>
+        /// <param name="gameId">Game entity to delete from</param>
+        Task DeleteSquadCheckIn(int SquadCheckInId, int gameId, int squadId);
+
+        /// <summary>
+        /// Updates a SquadCheckIn entity
+        /// </summary>
+        /// <param name="SquadCheckIn">Updated SquadCheckIn entity</param>
+        /// <param name="gameId">Game id</param>
+        /// <returns>Updated SquadCheckIn entity</returns>
+        Task<SquadCheckIn> UpdateSquadCheckIn(SquadCheckIn SquadCheckIn, int gameId, int squadId);
+    }
+}
